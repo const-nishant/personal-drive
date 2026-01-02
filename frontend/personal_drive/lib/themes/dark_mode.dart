eca2a0a045c44630486325870b3434f2958e2cc5
const-nishant <nishantspatil04@gmailcom>
2026-01-02 20:26:46 +0530

    feat: Implement dark and light theme support with ThemeProvider

---
+++ frontend/personal_drive/lib/themes/dark_mode.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+ThemeData darkTheme = ThemeData(
+  colorScheme: ColorScheme.dark(
+    surface: Colors.grey.shade900,
+    primary: Colors.grey.shade600,
+    secondary: Colors.grey.shade700,
+    tertiary: const Color.fromARGB(255, 57, 57, 57),
+    inversePrimary: Colors.grey.shade300,
+  ),
+  dialogTheme: DialogThemeData(
+    titleTextStyle: TextStyle(color: Colors.grey.shade600),
+    contentTextStyle: TextStyle(color: Colors.grey.shade600),
+    backgroundColor: Colors.grey.shade900,
+  ),
+  iconTheme: IconThemeData(color: Colors.grey.shade200),
+);
---
+++ frontend/personal_drive/lib/themes/light_mode.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+ThemeData lightTheme = ThemeData(
+  colorScheme: ColorScheme.light(
+    surface: Colors.grey.shade300,
+    primary: Colors.grey.shade500,
+    secondary: Colors.grey.shade200,
+    tertiary: Colors.white,
+    inversePrimary: Colors.grey.shade900,
+  ),
+  dialogTheme: DialogThemeData(
+    titleTextStyle: TextStyle(color: Colors.grey.shade500),
+    contentTextStyle: TextStyle(color: Colors.grey.shade500),
+  ),
+  iconButtonTheme: const IconButtonThemeData(
+    style: ButtonStyle(iconColor: WidgetStatePropertyAll(Colors.white)),
+  ),
+);
